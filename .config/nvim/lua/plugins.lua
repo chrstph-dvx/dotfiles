4f94b81572bb905755f7bd73e7f30c30db76c5d7
Christophe <contact@christophedeveaux.com>
2021-11-17 01:05:12 +0100

    Require lua from init.vim

---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,44 @@
+-- Run PackerCompile when this file is edited
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerCompile
+  augroup end
+]])
+
+-- Bootstrap packer (first time on any config)
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
+return require('packer').startup({function() 
+  use 'wbthomason/packer.nvim'
+
+  -- Easy motion
+  -- use 'easymotion/vim-easymotion'
+  -- use 'haya14busa/incsearch.vim'
+  -- use 'haya14busa/incsearch-fuzzy.vim'
+  -- use 'haya14busa/incsearch-easymotion.vim'
+
+  -- Statusline
+  -- use {
+  --  'hoob3rt/lualine.nvim',
+  --  requires = {'kyazdani42/nvim-web-devicons', opt = true},
+  -- }
+
+  
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end,
+-- Display packer in floating window
+config = {
+  display = {
+    open_fn = function()
+      return require('packer.util').float({ border = 'single' })
+    end
+  }
+}})
