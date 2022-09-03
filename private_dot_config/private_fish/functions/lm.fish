6e854451a99fe29514352ec37363342046c147f5
Christophe <contact@christophedeveaux.com>
2022-09-03 13:39:56 +0200

    Remove plugins functions, remove Lingoda functions

--- private_dot_config/private_fish/functions/lm.fish
+++
@@ -1,3 +0,0 @@
-function lm --wraps='make -s -C ~/dev/lingoda/linguando/devops/localdev/docker' --description 'alias lm=make -s -C ~/dev/lingoda/linguando/devops/localdev/docker'
-  make -s -C ~/dev/lingoda/linguando/devops/localdev/docker $argv; 
-end
--- private_dot_config/private_fish/functions/migrate.fish
+++
@@ -1,3 +0,0 @@
-function migrate --wraps='php bin/console doctrine:migration:migrate --no-interaction' --description 'alias migrate=php bin/console doctrine:migration:migrate --no-interaction'
-  php bin/console doctrine:migration:migrate --no-interaction $argv; 
-end
